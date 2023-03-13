ac5896e7e171f527781447c559233efd34ef8fb6
Johnson <mikesonowallah@gmail.com>
2023-03-13 13:28:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let answer;
+  if (ride <= 400) {
+    answer = "This one is on me!";
+  }
+  
+  if(ride > 2000) {
+    answer = 'I will gladly take your thirty bucks.';
+  }
+  
+  if(ride > 2500){
+    answer = 'No can do.';
+  }
+
+  
+  return answer;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  let answer;
+  if(city == "NYC"){
+    answer = `Ok, sounds good.`
+  }
+  else{
+    answer = 'No go.'
+  }
+
+  return answer
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let answer;
+  switch (tip) {
+    case "generous":
+      answer = "Thank you so much."
+      break;
+    case "not as generous":
+      answer = "Thank you."
+      break;
+    default:
+      answer = "Bye."
+      break;
+  }
+  return answer
 }
